ae75fa565a5aa186fe7910d9af0504639fc571aa
Justin Carter <jcarter@starkandwayne.com>
2016-12-23 12:15:55 +0100

    Read NOMAD_ALLOC_ID in app

--- app/server.rb
+++ app/server.rb
@@ -4,6 +4,8 @@ set :bind, '0.0.0.0'
 
 set :logging, true
 
+alloc_id = ENV['NOMAD_ALLOC_ID']
+
 get '/' do
-  'Hello ConfigManagementCamp from nomad!'
+  "Hello ConfigManagementCamp from nomad - NOMAD_ALLOC_ID: #{alloc_id}"
 end
